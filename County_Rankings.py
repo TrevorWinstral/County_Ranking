a095555bb88be6ef05da18d8537124605c2c3481
TrevorWinstral <trevtdw@gmail.com>
2020-07-14 16:55:54 +0200

    adjusted for first entry being first case

--- County_Rankings.py
+++ County_Rankings.py
@@ -112,7 +112,7 @@ for fip in fips:
         
     print(f'Working on {county}, {state}')
     frame.loc[:,'New']=frame.loc[:,'Cases'].diff()    
-    frame['New'] = frame['New'].fillna(0.0)
+    frame['New']=np.where(frame.New == np.NaN, frame.New, frame.Cases)
     frame.loc[:,'Total New Cases in Last 14 Days']=frame.loc[:,'New'].rolling(14, min_periods=1).sum()
     
     #Calculate Streak
