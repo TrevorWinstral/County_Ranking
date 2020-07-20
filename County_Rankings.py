4aee068620fc11a8a459bfe0026274df388f2050
vbrunsch <62330755+vbrunsch@users.noreply.github.com>
2020-07-20 02:50:33 -0400

    Update County_Rankings.py

--- County_Rankings.py
+++ County_Rankings.py
@@ -9,7 +9,7 @@ df.columns = ['Date', 'County', 'State', 'Fips', 'Cases', 'Deaths']
 df = df.sort_values(by=['State', 'County'])
 print(df.tail())
 
-#Assigning Fips to New York City, NY, Kansas City, MO and Joplin, MO
+# Assigning Fips to New York City, NY, Kansas City, MO and Joplin, MO
 df.loc[df['County'] == 'New York City', 'Fips'] = 999
 df.loc[df['County'] == 'Kansas City', 'Fips'] = 998
 df.loc[df['County'] == 'Joplin', 'Fips'] = 997
